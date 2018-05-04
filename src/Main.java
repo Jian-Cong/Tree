9d90c6e1011d459d978430dc0ffb7e2b1cc7c559
Jian-Cong <1978048527@qq.com>
2018-05-04 18:37:10 +0800

    删除节点，并修复整个树

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("a".compareTo("b"));
-        System.out.println("b".compareTo("a"));
-        System.out.println("a".compareTo("a"));
-
+        for(int i=0;i<10;i++){
+            System.out.print((int)(Math.random()*100)+",");
+        }
+        fun();
     }
+
+    public static void fun(){
+        for(int i=0;i<10;i++){
+            System.out.print((int)(Math.random()*100)+",");
+        }
+    }
+
 }
